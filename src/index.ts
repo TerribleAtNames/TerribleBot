1e552d1fbda060a2828e49f564deab1d63e29cf4
MrMythical <91077061+MrMythicalYT@users.noreply.github.com>
2022-02-17 09:15:07 -0500

    fixed index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.info("hey");
+import { Client, Intents } from "discord.js"; 
+import config from "dotenv";
+config.config();
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS]
+});
+
+client.login();
