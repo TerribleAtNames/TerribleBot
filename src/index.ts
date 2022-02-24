7db2311d31c212ce3e09cbfcc02060c8f51639cc
MrMythical <91077061+MrMythicalYT@users.noreply.github.com>
2022-02-24 10:58:15 -0500

    Update package.json and index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-import { Client, Collection, CommandInteraction, Intents } from "discord.js";
+import { Client, Collection, Intents, CommandInteraction } from "discord.js";
 import { readdirSync } from "fs";
+import "dotenv/config";
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+declare interface Command {
+    data: SlashCommandBuilder
+    execute: (interaction: CommandInteraction) => Promise<void>
+}
 
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS]
 });
-const commands: Collection<string, {name: string, execute: (interaction: CommandInteraction) => void}> = new Collection();
+const commands: Collection<string, Command> = new Collection();
 (async () => {
     for await (const i of readdirSync("./commands")) {
-        const file = await import(`./commands/${i}`);
-        commands.set(file.default.name, file.default);
+        const file: Command = await import(`../commands/${i}`);
+        commands.set(file.data.name, file);
     }
 
     client.on("interactionCreate", async interaction => {
